70659481d7aeae2ae913456269a79e49a23be9b4
Cat Wilber <catherine.wilber@gmail.com>
2020-04-28 16:57:29 -0500

    Update hello_name.py

--- 01_getting_set_up/hello_name.py
+++ 01_getting_set_up/hello_name.py
@@ -1,2 +1,6 @@
 user_name = input('Type your name, then press Enter: ')
-print('Hello,', user_name.upper().strip(), 'your name is', len(user_name.strip()), 'letters long' + '!')
+print('Hello', user_name.upper().strip(),'your name is' , len(user_name.strip()), 'letters long' + '!')
+
+username = input('Your name: ')
+length = len(username)
+print('Hello', username.upper().strip(),',' 'your name is',length, 'letters long.')
