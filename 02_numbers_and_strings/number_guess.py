2925303fb94218a94284088726ebfa39dbb07d04
Cat Wilber <catherine.wilber@gmail.com>
2020-04-28 16:18:55 -0500

    Update number_guess.py

--- 02_numbers_and_strings/number_guess.py
+++ 02_numbers_and_strings/number_guess.py
@@ -1,1 +1,9 @@
-# Your code here...
+x = 20 
+number = input('Type your number, then press Enter: ')
+if float(number) > x:
+    print ('Your number is greater than mine.')
+elif float(number) < x:
+    print ('Your number is less than mine.')
+else:
+    print('Your number equals my number!')
+
